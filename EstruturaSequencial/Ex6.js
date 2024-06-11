fcacb6b4a07a3cf5c1dadcd3f004753e5e0f935b
Gabriel Melo <bielrj170@gmail.com>
2024-06-11 19:13:17 -0300

    adicionando comentários

--- EstruturaSequencial/Ex6.js
+++ EstruturaSequencial/Ex6.js
@@ -1,11 +1,11 @@
 // Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
 const prompt = require('readline-sync')
 
-function areaQuadrado(l) {
-    return Math.pow(l,2)
+function areaQuadrado(l) { // function para área do quadrado
+    return Math.pow(l,2) // biblioteca Math para Potenciação
 }
 
-const lado = Number(prompt.question("Qual o lado desse quadrado?"))
-const resultado = areaQuadrado(lado) * 2
+const lado = Number(prompt.question("Qual o lado desse quadrado?")) // perguntando lado pro usuário
+const resultado = areaQuadrado(lado) * 2 // dobro da área
 
-console.log(`O dobro da área do quadrado é: ${resultado}.`)
+console.log(`O dobro da área do quadrado é: ${resultado}.`) // exibindo usuário
