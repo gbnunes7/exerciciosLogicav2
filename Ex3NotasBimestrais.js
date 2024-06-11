49b5342e5055ad2881497734eba2853a27f86039
Gabriel Melo <bielrj170@gmail.com>
2024-06-10 21:12:06 -0300

    Adicionando comentários

--- Ex3NotasBimestrais.js
+++ Ex3NotasBimestrais.js
@@ -1,19 +1,19 @@
 const prompt = require('readline-sync')
 // Faça um Programa que peça as 4 notas bimestrais e mostre a média.
 
-const notas = []
-let limitador = true
+const notas = [] // Array de Notas
+let limitador = true // Limitador do Loop 
 
-while(limitador) {
-    const nota = Number(prompt.question("Qual foi a nota bimestral?"))
-    notas.push(nota)
-    const continuar = prompt.question("Deseja continuar inserindo?")
-    if(continuar.toLowerCase() !== "sim") {
-        limitador = false 
+while(limitador) { // loop caso limitador seja true
+    const nota = Number(prompt.question("Qual foi a nota bimestral?")) // nota a ser inserida
+    notas.push(nota) // inserindo nota para o array
+    const continuar = prompt.question("Deseja continuar inserindo?") // variavel para declarar se continua ou não
+    if(continuar.toLowerCase() !== "sim") { // condicional para decisao se continua botando nota ou não
+        limitador = false // se não, limitador igual false e acaba com o loop
     }
 }
 
-const notasSomadas = notas.reduce((acumulador, valorAtual) => {
+const notasSomadas = notas.reduce((acumulador, valorAtual) => { // expressão da biblioteca Math para somar todos valores de um array
     return acumulador + valorAtual;
   }, 0);
   
