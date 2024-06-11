9785b762a488bcab80b713696bb8fd5fbb45e576
Gabriel Melo <bielrj170@gmail.com>
2024-06-10 21:32:01 -0300

    quinto exercício

--- 5-RaioCirculo.js
+++ 5-RaioCirculo.js
@@ -1,3 +1,4 @@
+// Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
 const prompt = require('readline-sync') 
 
 const raio = Number(prompt.question("Qual o raio do circulo?"))
@@ -5,7 +6,7 @@ let unidade = prompt.question("Qual a unidade? Cm/M")
 unidade.toLowerCase() == "cm" ? unidade = "cm" : unidade = "m" 
 
 function areaCirculo(r) {
-    return Math.PI * Math.pow(r, 2)
+    return (Math.PI.toFixed(2)) * (Math.pow(r, 2))
 }
 
 console.log(`A área do circulo é ${areaCirculo(raio)} ${unidade}².`)
