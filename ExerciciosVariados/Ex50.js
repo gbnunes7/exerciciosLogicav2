ed57396bd0d69f6bcf4d757cb0dd0dc2c3637b18
Gabriel Melo <bielrj170@gmail.com>
2024-06-30 17:59:18 -0300

    pilha binária

---
+++ ExerciciosVariados/Ex50.js
@@ -0,0 +1,40 @@
+
+class Stack {
+    constructor(){
+        this.items = []
+    }
+
+    add(element) {
+        this.items.push(element)
+    }
+
+    remove() {
+        return this.items.pop()
+    }
+
+    isEmpty() {
+        return this.items.length === 0
+    }
+}
+
+
+
+function decimalToBinary(decNumber) {
+    const stackBinary = new Stack() // instanciando minha classe stack
+    let number = decNumber; // atribuindo meu numero decimal a um numero
+    let rem; // inicialziando variavel de resto
+    let binaryString = ''; // incializando variavel para string
+    while(number > 0) { // lógica da conversão para binario
+        rem = Math.floor(number % 2); // rem é igual ao resto da operação inicial
+        stackBinary.add(rem); // push resto para a stack
+        number = Math.floor(number / 2); // depois o numero é divido por 2 e repete a lógica
+    }
+    console.log(stackBinary)
+    while (!stackBinary.isEmpty()) {
+        binaryString += stackBinary.remove().toString() // a stack vai sendo removida e adicionada a binarystring pelo principio lifo
+    }
+    
+    return binaryString
+}
+
+console.log(decimalToBinary(233))
