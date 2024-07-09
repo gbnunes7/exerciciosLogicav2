8c1f4bdbb23fea344d82952e7d3fd0e9b5e15d00
Gabriel Melo <bielrj170@gmail.com>
2024-07-09 18:29:49 -0300

    Exercicio

---
+++ ExerciciosVariados/Ex99.js
@@ -0,0 +1,10 @@
+var number=function(array){
+    let arrNovo = []
+    for(let i = 1 ; i <= array.length ; i++) {
+        arrNovo.push(`${i}:${array[i-1]}`)
+    }
+    return arrNovo
+  }
+
+
+  console.log(number(["a", "b", "c"])) //["1: a", "2: b", "3: c"]
