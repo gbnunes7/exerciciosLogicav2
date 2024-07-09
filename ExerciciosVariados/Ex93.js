8c3bbbc64fb766b09c4621fe50d9c865ab4f52c7
Gabriel Melo <bielrj170@gmail.com>
2024-07-08 23:38:02 -0300

    exercicio

---
+++ ExerciciosVariados/Ex93.js
@@ -0,0 +1,7 @@
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    console.log(birds.filter((bird) => !geese.includes(bird)))
+    
+  };
+
+  gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"])
