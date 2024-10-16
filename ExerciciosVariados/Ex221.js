9ba81c51004fb128a9932fc1dec812d9433209d5
Gabriel Melo <bielrj170@gmail.com>
2024-10-15 23:21:35 -0300

    .

--- ExerciciosVariados/Ex221.js
+++ ExerciciosVariados/Ex221.js
@@ -1,4 +1,5 @@
 const fibonacciRecursivo = (n) => {
-    if (n < 2) return n;
-    return fibonacciRecursivo(n - 1) + fibonacciRecursivo(n - 2);
+	if (n < 2) return n;
+    console.log(teste)
+	return fibonacciRecursivo(n - 1) + fibonacciRecursivo(n - 2);
 };
